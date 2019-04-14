82d26f20e1419b43df2be01377cde7e4fadfb6a9
Jamie Pham <jtkp66@gmail.com>
2019-04-13 23:39:31 -0700

    Added Route files

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+// Auth
+const express = require("express");
+const router = express.Router();
+
+// @route GET api/users/test
+// @descr Test user route
+// @access Public
+router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+
+module.exports = router;
