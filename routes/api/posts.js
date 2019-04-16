125c9928e52533c0c377a42b2f50431c3eab9982
Jamie Pham <jtkp66@gmail.com>
2019-04-16 13:30:14 -0700

    Added route to delete comments. Backend API complete.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -186,4 +186,38 @@ router.post(
   }
 );
 
+// @route DELETE api/posts/comment/:id/:comment_id
+// @descr Delete comment from a Post
+// @access Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check to see if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexist: "Comment doesn't exist" });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        // Splice out of array
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
 module.exports = router;
