d492c532ba7e79aba68d4ffad5febe817abb33c1
Jamie Pham <jtkp66@gmail.com>
2019-04-16 17:43:06 -0700

    Added Registration component.

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,31 +1,27 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <div className="landing">
-          <div className="dark-overlay landing-inner text-light">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-12 text-center">
-                  <h1 className="display-3 mb-4">Origin Connect</h1>
-                  <p className="lead">
-                    {" "}
-                    Create a developer profile/portfolio, share posts and get
-                    help from other Origin Alumni
-                  </p>
-                  <hr />
-                  <a
-                    href="register.html"
-                    className="btn btn-lg btn-secondary mr-2"
-                  >
-                    Sign Up
-                  </a>
-                  <a href="login.html" className="btn btn-lg btn-light">
-                    Login
-                  </a>
-                </div>
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Origin Connect</h1>
+                <p className="lead">
+                  {" "}
+                  Create a developer profile/portfolio, share posts and get help
+                  from other Origin Alumni
+                </p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-secondary mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
               </div>
             </div>
           </div>
