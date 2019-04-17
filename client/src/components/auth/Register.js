9404fb1a1c751789628d8ec5698953ecb68650b4
Jamie Pham <jtkp66@gmail.com>
2019-04-17 14:51:38 -0700

    Created redux store snd redux boilerplate

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -50,7 +50,7 @@ class Register extends Component {
               <p className="lead text-center">
                 Create your OriginConnect account
               </p>
-              <form onSubmit={this.onSubmit}>
+              <form noValidate onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
