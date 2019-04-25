e43bd2f50600cf41b8e6e53c90c98b5237be18df
Jamie Pham <jtkp66@gmail.com>
2019-04-25 14:12:48 -0700

    Created action to add comments, put them in state, and save to the database.

--- validation/post.js
+++ validation/post.js
@@ -7,7 +7,7 @@ module.exports = function validatePostInput(data) {
   data.text = !isEmpty(data.text) ? data.text : "";
 
   if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
-    errors.text = "Post must bbe betweenn 10 and 300 characters ";
+    errors.text = "Post must be betweenn 10 and 300 characters ";
   }
 
   if (Validator.isEmpty(data.text)) {
